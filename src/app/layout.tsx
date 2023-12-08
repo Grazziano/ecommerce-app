d20066ec4d6883ef08f1e9b68873739264753293
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2023-12-08 18:21:08 -0300

    Installing node modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Ecommerce',
+  title: 'Dev Shop - Ecommerce',
   description: 'An ecommerce site build with NextJS and Typescript.',
 };
 
