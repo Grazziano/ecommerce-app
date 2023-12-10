eac8bd48af6b7fce4f6551bf8f17d9eb4a26ff42
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2023-12-10 20:01:26 -0300

    User model

---
+++ src/models/userModel.ts
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+export const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    deliveryAddresses: {
+      type: Array,
+      default: [],
+      required: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+      required: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// if the model is already defined, use that model else create a new one
+// const User = mongoose.models.User || mongoose.model('User', userSchema);
+let User = null;
+
+if (mongoose.models['users']) {
+  User = mongoose.models['users'];
+} else {
+  User = mongoose.model('users', userSchema);
+}
+
+export default User;
