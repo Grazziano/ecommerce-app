1c1f6ba62d6e0a1cb5556b01418f52578fac7af6
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2023-12-11 18:22:36 -0300

    Profile page UI

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,39 @@
+'use client';
 import React from 'react';
 import { Tabs } from 'antd';
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state: any) => state.user);
+  return (
+    <div>
+      {currentUser.isAdmin && (
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab="Products" key="1">
+            Products
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Categories" key="2">
+            Categories
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Orders" key="3">
+            Orders
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Users" key="4">
+            Users
+          </Tabs.TabPane>
+        </Tabs>
+      )}
+
+      {!currentUser.isAdmin && (
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab="Orders" key="1">
+            Orders
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Personal Information" key="2">
+            Personal Information
+          </Tabs.TabPane>
+        </Tabs>
+      )}
+    </div>
+  );
 }
