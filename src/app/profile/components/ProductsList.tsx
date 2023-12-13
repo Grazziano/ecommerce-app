5194b365d42f660e1caeeacf1cec8956cf971813
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-12-13 14:04:34 -0300

    Show products

--- src/app/profile/components/ProductsList.tsx
+++ src/app/profile/components/ProductsList.tsx
@@ -1,9 +1,75 @@
-import React from 'react';
-import { Button } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Button, Table, message } from 'antd';
+import axios from 'axios';
+import moment from 'moment';
 import { useRouter } from 'next/navigation';
 
 export default function ProductsList() {
   const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('/api/products');
+      setProducts(response.data.data);
+    } catch (error: any) {
+      message.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const columns = [
+    {
+      title: 'Product',
+      dataIndex: 'name',
+      render: (text: any, record: any) => (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={record.images[0]}
+          alt={text}
+          className="w-20 h-20 object-cover rounded-full"
+        />
+      ),
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Created By',
+      dataIndex: 'createdBy',
+      render: (createdBy: any) => createdBy.name,
+    },
+    {
+      title: 'Created At',
+      dataIndex: 'createdAt',
+      render: (createdAt: any) =>
+        moment(createdAt).format('DD MM YYYY hh:mm A'),
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      render: (action: any, params: any) => {
+        return (
+          <div className="flex gap-3 items-center">
+            <Button type="default" className="btn-small" onClick={() => {}}>
+              Delete
+            </Button>
+            <Button type="primary" className="btn-small" onClick={() => {}}>
+              Edit
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
 
   return (
     <div>
@@ -14,6 +80,15 @@ export default function ProductsList() {
         >
           Add Product
         </Button>
+      </div>
+
+      <div className="mt-5">
+        <Table
+          columns={columns}
+          dataSource={products}
+          loading={loading}
+          pagination={false}
+        />
       </div>
     </div>
   );
