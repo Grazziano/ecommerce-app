15f31096a312061b27354a7021383d5d9279cdfa
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-12-18 10:15:30 -0300

    Review model and api

---
+++ src/app/api/reviews/route.ts
@@ -0,0 +1,41 @@
+import { connectDB } from '@/configs/dbConfig';
+import { validateJWT } from '@/helpers/validateJWT';
+import Review from '@/models/reviewModel';
+import { NextRequest, NextResponse } from 'next/server';
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    await validateJWT(request);
+
+    const reqBody = await request.json();
+
+    const newReview = new Review(reqBody);
+
+    await newReview.save();
+
+    return NextResponse.json({
+      message: 'Review added successfully',
+      review: newReview,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    await validateJWT(request);
+
+    const searchParams = request.nextUrl.searchParams;
+
+    const product = searchParams.get('product');
+
+    const reviews = await Review.find({ product });
+
+    return NextResponse.json(reviews);
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
