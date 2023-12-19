2542d98e173d6b443015a0b65db95de1a2b4ff5b
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-12-19 11:59:07 -0300

    User update API

--- src/app/api/users/[userid]/route.ts
+++ src/app/api/users/[userid]/route.ts
@@ -1,4 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
+import User from '@/models/userModel';
+import { connectDB } from '@/configs/dbConfig';
+import bcryptjs from 'bcryptjs';
+
+connectDB();
 
 interface Params {
   params: {
@@ -18,4 +23,41 @@ export async function GET(request: NextRequest, { params }: Params) {
       },
     ],
   });
+}
+
+export async function PUT(request: NextRequest, { params }: Params) {
+  try {
+    const userId = params.userid;
+
+    // check if existing password is correct
+    const reqBody = await request.json();
+    const user = await User.findOne({ email: reqBody.email });
+
+    const isMatch = await bcryptjs.compare(
+      reqBody.password,
+      user.password as string
+    );
+
+    if (!isMatch) {
+      return NextResponse.json(
+        { message: 'Invalid old password' },
+        { status: 401 }
+      );
+    }
+
+    // update password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(reqBody.newPassword, salt);
+
+    reqBody.password = hashedPassword;
+
+    await User.findByIdAndUpdate(userId, reqBody);
+
+    return NextResponse.json(
+      { message: 'User updated successfully' },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
